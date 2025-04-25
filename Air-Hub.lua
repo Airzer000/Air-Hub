031e59e522ab62c0650cae13dc15ae3be6b529b6
Airzer000 <airzero000.pro@gmail.com>
2025-04-25 17:43:15 -0300

    Update and rename Source Code.lua to Air-Hub.lua

--- Air-Hub.lua
+++ Air-Hub.lua
@@ -5,8 +5,8 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
+   LoadingTitle = "Airhub",
+   LoadingSubtitle = "by (Discord) Diego_144",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    DisableRayfieldPrompts = false,

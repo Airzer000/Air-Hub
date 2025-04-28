5cbb3a0ef792030dd334b510d1130de93d4deda3
Airzer000 <airzero000.pro@gmail.com>
2025-04-28 17:30:30 -0300

    Create Settings.lua

---
+++ Modules/Settings.lua
@@ -0,0 +1,13 @@
+local Settings = {
+    Debug = true,
+    Smoothness = 0.2,
+    FOV = 40,
+    MaxDistance = 400,
+    TeamCheck = false,
+    WallCheck = false,
+    AimPart = "Head",
+    AimbotEnabled = false,
+    ESPEnabled = false
+}
+
+return Settings

5aa464f45e6ea13ca2c20f28fa6c9e42130b30e2
Airzer000 <airzero000.pro@gmail.com>
2025-04-28 17:49:07 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,18 @@
-local repo = "Airzer000/Air-Hub" -- Nome do seu repositório
-local branch = "main" -- Ou o nome da branch onde você está
+-- Defina o repositório e a branch
+local repo = "Airzer000/Air-Hub"
+local branch = "main"
 local baseUrl = "https://raw.githubusercontent.com/"..repo.."/"..branch.."/"
 
+-- Função para carregar módulos
 local function loadModule(modulePath)
     return loadstring(game:HttpGet(baseUrl..modulePath))()
 end
 
--- Carregar o Main.lua e outros módulos
-loadstring(game:HttpGet(baseUrl.."Main.lua"))()
+-- Carregando módulos específicos (exemplo)
+loadModule("Main.lua") -- Carregar o Main.lua
+
+-- Carregar os módulos adicionais
+loadModule("Modules/Aimbot.lua")
+loadModule("Modules/ESP.lua")
+loadModule("Modules/Settings.lua")
+loadModule("Modules/Utils.lua")
